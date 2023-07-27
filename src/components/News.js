c625ac3a95ab73c9f36cb7643f86b3c7aeb5c490
guptaji1008 <132588314+guptaji1008@users.noreply.github.com>
2023-07-27 17:23:42 +0530

    Made newsComponents with the help of bootstrap

--- src/components/News.js
+++ src/components/News.js
@@ -5,14 +5,20 @@ export class News extends Component {
   render() {
     return (
       <div>
-        This is a news panel where all the news headlines will be reflected.
-        <NewsItems/>
-        <NewsItems/>
-        <NewsItems/>
-        <NewsItems/>
-        <NewsItems/>
-        <NewsItems/>
-        <NewsItems/>
+        <h1 className='my-3'>Top News - Headlines</h1>
+        <div className="container my-5">
+            <div className="row">
+                <div className="col-md-4">
+                    <NewsItems title="My Title" description="My Description"/>
+                </div>
+                <div className="col-md-4">
+                    <NewsItems title="My Title" description="My Description"/>
+                </div>
+                <div className="col-md-4">
+                    <NewsItems title="My Title" description="My Description"/>
+                </div>
+            </div>
+        </div>
       </div>
     )
   }
--- src/components/newsItems.js
+++ src/components/newsItems.js
@@ -2,9 +2,17 @@ import React, { Component } from 'react'
 
 export class newsItems extends Component {
   render() {
+      let {title, description} = this.props
     return (
       <div>
-        This is a news items which will be reflected on News panel.
+        <div className="card" style={{width: "18rem"}}>
+            <img src="..." className="card-img-top" alt="..."/>
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text">{description}</p>
+                <a href="/newsItems" className="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
       </div>
     )
   }
