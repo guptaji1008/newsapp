a8e7f57047e5f9f80290fe7e87571b7f219f25bf
guptaji1008 <132588314+guptaji1008@users.noreply.github.com>
2023-07-27 14:10:05 +0530

    Added new js files

---
+++ src/components/News.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import NewsItems from './NewsItems'
+
+export class News extends Component {
+  render() {
+    return (
+      <div>
+        This is a news panel where all the news headlines will be reflected.
+        <NewsItems/>
+        <NewsItems/>
+        <NewsItems/>
+        <NewsItems/>
+        <NewsItems/>
+        <NewsItems/>
+        <NewsItems/>
+      </div>
+    )
+  }
+}
+
+export default News
---
+++ src/components/newsItems.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class NewsItems extends Component {
+  render() {
+    return (
+      <div>
+        This is a news items which will be reflected on News panel.
+      </div>
+    )
+  }
+}
+
+export default NewsItems
